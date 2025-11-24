ce0786c1c88dceab1ea81d016cadb0faca0842a9
Sarztak <sar3142168@gmail.com>
2025-11-24 01:54:19 -0600

    added deck.js and changed name to index.html

---
+++ deck.js
@@ -0,0 +1,10 @@
+const deckData = [
+    { "hanzi": "我", "pinyin": "wo3", "english": "I; me" },
+    { "hanzi": "你", "pinyin": "ni3", "english": "you" },
+    { "hanzi": "好", "pinyin": "hao3", "english": "good" },
+    { "hanzi": "是", "pinyin": "shi4", "english": "to be" },
+    { "hanzi": "吗", "pinyin": "ma5", "english": "question particle" },
+    { "hanzi": "很", "pinyin": "hen3", "english": "very" },
+    { "hanzi": "谢谢", "pinyin": "xie4 xie5", "english": "thank you" },
+    { "hanzi": "中国", "pinyin": "zhong1 guo2", "english": "China" }
+]
--- script.js
+++ script.js
@@ -1,13 +1,4 @@
-const deck = [
-    { hanzi: "我", pinyin: "wo3", english: "I; me" },
-    { hanzi: "你", pinyin: "ni3", english: "you" },
-    { hanzi: "好", pinyin: "hao3", english: "good" },
-    { hanzi: "是", pinyin: "shi4", english: "to be" },
-    { hanzi: "吗", pinyin: "ma5", english: "question particle" },
-    { hanzi: "很", pinyin: "hen3", english: "very" },
-    { hanzi: "谢谢", pinyin: "xie4 xie5", english: "thank you" },
-    { hanzi: "中国", pinyin: "zhong1 guo2", english: "China" }
-];
+let deck = [...deckData];
 
 function shuffle(array) {
     for (let i = array.length - 1; i > 0; i--) {
